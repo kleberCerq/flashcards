b9eaf8a99d1a3613b278facd5750db53396d394f
kleberCerq <klebercerqueira07@gmail.com>
2024-06-01 11:07:01 -0300

    Color selector fixed
    Sistema de seleção de cores agora funciona normalmente.

--- src/js/script.js
+++ src/js/script.js
@@ -11,3 +11,23 @@ cancelBtn.addEventListener("click", function () {
   container.style.display = "none";
   btn.style.display = "block";
 });
+
+var colorsPalette = document.getElementsByClassName("color-selector");
+var colors1 = colorsPalette[0];
+var colors2 = colorsPalette[1];
+
+function dropContent() {
+  if (colors1.style.display === "grid") {
+    colors1.style.display = "none";
+  } else {
+    colors1.style.display = "grid";
+  }
+}
+
+function dropContent2() {
+  if (colors2.style.display === "grid") {
+    colors2.style.display = "none";
+  } else {
+    colors2.style.display = "grid";
+  }
+}
